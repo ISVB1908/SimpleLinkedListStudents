f38d350a607c5afbaef0b073bca5acd1b0d3d777
ivan vasquez <ivan.vasquez.2020@upb.edu.co>
2021-08-18 19:20:29 -0500

    Simple Linked list Estructure+func

--- src/main/java/com/mycompany/studentsimplelist/Main.java
+++ src/main/java/com/mycompany/studentsimplelist/Main.java
@@ -5,10 +5,24 @@
  */
 package com.mycompany.studentsimplelist;
 
+import com.mycompany.classes.SList;
+
 /**
  *
  * @author vasqu
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        SList list = new SList();
+        list.add("perra", 0);
+        list.add("carlos", 0);
+        list.add("david", 5);
+        list.add("ana", 0);
+        list.add("bobo", 0);
+        list.add("aaron", 0);
+        list.add("x",0);
+        list.add("aaron", 4);
+        list.add("bobo", 0);
+        list.print();
+    }
 }
